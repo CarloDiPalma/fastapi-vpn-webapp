f95d912bb27dbeeffe808f6920e5380f97e56a34
Pavel <ping.06@mail.ru>
2024-06-21 16:22:35 +0300

    Migration is trial

--- app/models.py
+++ app/models.py
@@ -11,7 +11,6 @@ user = Table(
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey(role.c.id)),
     Column("hashed_password", String, nullable=False),
     Column("is_active", Boolean, default=True, nullable=False),
     Column("is_superuser", Boolean, default=False, nullable=False),
