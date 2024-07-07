19a24ca33f01da58b4b5cc2c0bd3d6bbbaaaf90b
CarloDiPalma <ping.06@mail.ru>
2024-07-07 13:13:18 +0300

    Edit tests - add fixture

--- app/test_main.py
+++ app/test_main.py
@@ -7,6 +7,9 @@ from app.db import Base, engine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+from app.models import User
+from app.utils import generate_custom_token
+
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 TestSessionLocal = sessionmaker(
     autocommit=False,
@@ -25,7 +28,10 @@ app.dependency_overrides[get_db] = override_get_db
 
 @pytest_asyncio.fixture(scope="module")
 async def async_client():
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test"
+    ) as client:
         async with engine.begin() as conn:
             # Создаем таблицы перед тестами
             await conn.run_sync(Base.metadata.create_all)
@@ -35,11 +41,25 @@ async def async_client():
             await conn.run_sync(Base.metadata.drop_all)
 
 
+@pytest_asyncio.fixture(scope="module")
+async def auth_token(async_client):
+    async with TestSessionLocal() as session:
+        # Создаем тестового пользователя
+        user = User(tg_id=10, username="TEST_USER", full_name="TEST_FULL_NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        # Создаем токен для этого пользователя
+        access_token = await generate_custom_token(user)
+        return access_token.get('token')
+
+
 @pytest.mark.asyncio
-async def test_create_protocol(async_client):
+async def test_create_protocol(async_client, auth_token):
     response = await async_client.post(
         "/protocol",
-        json={"name": "Test Protocol"}
+        json={"name": "Test Protocol"},
+        headers={"Authorization": f"Bearer {auth_token}"}
     )
     assert response.status_code == 200
     data = response.json()
