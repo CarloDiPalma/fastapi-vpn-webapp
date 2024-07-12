3582adce6b8db17700a833f3a5c15f9ebf70ca8f
CarloDiPalma <ping.06@mail.ru>
2024-07-12 17:52:18 +0300

    Add bot and telegram html page

---
+++ bot/run.py
@@ -0,0 +1,52 @@
+import asyncio
+import logging
+import os
+import sys
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import (
+    Message, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+)
+from dotenv import load_dotenv
+
+from aiogram.filters import CommandStart
+from aiogram import Router
+
+router = Router()
+
+buttons = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(
+            text='ЗДЕСЬ WEBAPP!',
+            web_app=WebAppInfo(url='http://95.214.9.102/telegram.html')
+        )]
+    ]
+)
+
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """This handler receives messages with `/start` command."""
+    await message.answer(
+        f"Hello, {message.from_user.full_name}!",
+        reply_markup=buttons
+    )
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+
+async def main() -> None:
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit!')
