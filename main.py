d7519a911a5c48486e99f8feecbd01156ed703a9
CarloDiPalma <ping.06@mail.ru>
2024-07-15 21:03:34 +0300

    Change fastapi port to 8080

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="127.0.0.1", log_level="info", reload=True)
+    uvicorn.run(
+        "app.app:app",
+        host="127.0.0.1",
+        log_level="info",
+        reload=True,
+        port=8080
+    )
